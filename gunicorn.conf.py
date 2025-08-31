45209f716f681664e3e2079b36ab02c389ec3ef9
shawz1-phper <ywaftkhr971@gmail.com>
2025-08-31 20:47:19 +0300

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import multiprocessing
 
 bind = "0.0.0.0:{}".format(os.environ.get('PORT', 10000))
 workers = 1
-worker_class = "eventlet"
+worker_class = "gthread"
 timeout = 120
 keepalive = 5
 loglevel = "info"
